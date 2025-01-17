2b89a10cc92d63e4f0123de7ab26431830e0825b
Himmu9131 <hdwivedi96@gmail.com>
2025-01-17 15:37:37 +0530

    added exp

---
+++ book-my-show-exp/book-my-show-exp/src/main/java/com/bookmyshow_experience/book_my_show_exp/BookMyShowExpApplication.java
@@ -0,0 +1,13 @@
+package com.bookmyshow_experience.book_my_show_exp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowExpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMyShowExpApplication.class, args);
+	}
+
+}
---
+++ book-my-show-exp/book-my-show-exp/src/test/java/com/bookmyshow_experience/book_my_show_exp/BookMyShowExpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmyshow_experience.book_my_show_exp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowExpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
